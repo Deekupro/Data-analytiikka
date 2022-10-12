f1f6f45bd989ba6c4a17554f64095a0cf241b724
topielias <topi.leskinen@tuni.fi>
2022-10-12 18:14:52 +0300

    Lisätty .gitignore, data/ kansio ignorataan

--- Data-harkka.py
+++ Data-harkka.py
@@ -1,4 +1,9 @@
-#tämä on testi 
-
 import pandas as pd
 
+
+
+
+
+df1 = pd.read_csv('accidents_2005_to_2007.csv')
+df2 = pd.read_csv('accidents_2009_to_2011.csv')
+df3 = pd.read_csv('accidents_2012_to_2014.csv')
